1bca3e9482fe3cf4cf2d12b7bdc4787e4bd21f45
Vanessa Maldonado <vbarajas4@gmail.com>
2021-08-08 22:58:48 -0700

    adding Apollo functionality

--- server/server.js
+++ server/server.js
@@ -1,7 +1,29 @@
 const express = require('express');
+
+// Import the ApolloServer class
+const { ApolloServer } = require('apollo-server-express');
+
 const path = require('path');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+
 const db = require('./config/connection');
 const routes = require('./routes');
+
+// Import `authMiddleware()` function to be configured with the Apollo Server
+const { authMiddleware } = require('./utils/auth');
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
+  context: authMiddleware,
+});
+
+// Update Express.js to use Apollo server features
+server.applyMiddleware({ app });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,5 +39,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+  console.log(`🌍 API server running on port ${PORT}!`);
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
