3c8f51ba8b8df24f3ceea43cba15eb5b77de5b63
Vanessa Maldonado <vbarajas4@gmail.com>
2021-08-11 22:38:15 -0700

    setting mutation for front end

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -42,7 +42,7 @@ export const SAVE_BOOK = gql`
   }
 `;
 
-export const REMOVE_SKILL = gql`
+export const REMOVE_BOOK = gql`
   mutation removeBook($bookId: String!) {
     removeBook(bookId: $bookId) {
       _id
