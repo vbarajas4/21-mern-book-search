0b0a8725ffd77d67a81973a82b686c92c7fa3824
Vanessa Maldonado <vbarajas4@gmail.com>
2021-08-14 09:47:33 -0700

    uncommented a throw error

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -75,9 +75,9 @@ const SearchBooks = () => {
     try {
       const response = await saveBook({ variables: { bookToSave }});
 
-      // if (!response.ok) {
-      //   throw new Error('something went wrong!');
-      // }
+      if (!response.ok) {
+        throw new Error('something went wrong!');
+      }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
